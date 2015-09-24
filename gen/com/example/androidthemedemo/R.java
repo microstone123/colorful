95c9098f8934d95b45ec4eb3d542ac629deee30b
stonelin <lin_haishi@qq.com>
2015-09-24 09:17:03 +0800

    [优化项目]

--- gen/com/example/androidthemedemo/R.java
+++ gen/com/example/androidthemedemo/R.java
@@ -30,14 +30,31 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int text_color=0x7f010002;
     }
     public static final class color {
+        public static final int activity_bg_color=0x7f040004;
+        public static final int addembl_cor=0x7f040002;
         /**  夜间模式 
          */
-        public static final int black_btn_color=0x7f040002;
-        public static final int black_tx_color=0x7f040001;
+        public static final int black_btn_color=0x7f040011;
+        public static final int black_tx_color=0x7f040010;
+        /**  bottom color 
+         */
+        public static final int bottom_title_bg=0x7f040003;
+        public static final int category_tab_highlight_bg=0x7f04000c;
+        public static final int category_tab_highlight_dark_bg=0x7f04000d;
+        public static final int category_tab_highlight_text=0x7f04000a;
+        public static final int category_tab_highlight_text_dark=0x7f04000b;
+        public static final int category_tab_text=0x7f040008;
+        public static final int category_tab_text_dark=0x7f040009;
+        public static final int divider=0x7f040006;
+        public static final int drop_down_list_footer_font_color=0x7f04000e;
+        public static final int gray=0x7f040007;
+        public static final int night=0x7f040001;
+        public static final int top_category_bar_background=0x7f040005;
+        public static final int white=0x7f040000;
         /**  日间模式 
          */
-        public static final int white_btn_color=0x7f040000;
-        public static final int white_tx_color=0x7f040003;
+        public static final int white_btn_color=0x7f04000f;
+        public static final int white_tx_color=0x7f040012;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -47,33 +64,61 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f050001;
+        public static final int activity_vertical_margin=0x7f050002;
+        public static final int category_tab_text=0x7f050004;
+        public static final int drop_down_list_footer_font_size=0x7f050007;
+        public static final int main_top_left=0x7f050000;
+        public static final int title_bar_height=0x7f050005;
+        public static final int top_category_height=0x7f050006;
+        public static final int top_height=0x7f050003;
+        public static final int various_head_height=0x7f050008;
     }
     public static final class drawable {
         public static final int bg_day=0x7f020000;
         public static final int bg_night=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int load_1=0x7f020003;
+        public static final int load_10=0x7f020004;
+        public static final int load_11=0x7f020005;
+        public static final int load_12=0x7f020006;
+        public static final int load_2=0x7f020007;
+        public static final int load_3=0x7f020008;
+        public static final int load_4=0x7f020009;
+        public static final int load_5=0x7f02000a;
+        public static final int load_6=0x7f02000b;
+        public static final int load_7=0x7f02000c;
+        public static final int load_8=0x7f02000d;
+        public static final int load_9=0x7f02000e;
+        public static final int loading_progress_bg=0x7f02000f;
     }
     public static final class id {
         public static final int change_btn=0x7f080002;
+        public static final int drop_down_list_footer_button=0x7f080008;
+        public static final int drop_down_list_footer_progress_bar=0x7f080007;
+        public static final int list_footer=0x7f080006;
         public static final int listview=0x7f080004;
-        public static final int news_creator=0x7f080006;
-        public static final int news_title=0x7f080007;
+        public static final int news_creator=0x7f080009;
+        public static final int news_title=0x7f08000a;
+        public static final int picfragment=0x7f08000c;
         public static final int recycler_view=0x7f080005;
         public static final int root_view=0x7f080000;
         public static final int second_btn=0x7f080003;
         public static final int textview=0x7f080001;
+        public static final int various_head_layout=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_recycler=0x7f030001;
-        public static final int news_lv_item=0x7f030002;
+        public static final int drop_down_list_footer=0x7f030002;
+        public static final int news_lv_item=0x7f030003;
+        public static final int swipe_head=0x7f030004;
     }
     public static final class string {
         public static final int action_settings=0x7f060003;
         public static final int app_name=0x7f060000;
         public static final int change_theme=0x7f060001;
+        public static final int drop_down_list_footer_default_text=0x7f060004;
         public static final int sec_act=0x7f060002;
     }
     public static final class style {
@@ -89,7 +134,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070003;
+        public static final int AppBaseTheme=0x7f070004;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -104,5 +149,6 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         /**  夜间主题 
          */
         public static final int NightTheme=0x7f070002;
+        public static final int drop_down_list_footer_font_style=0x7f070003;
     }
 }
